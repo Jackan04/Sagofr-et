7a579c17dcd371e74d0cfaad6952811ea90c01ab
Jackan04 <jacob.asker@gmail.com>
2024-11-03 09:59:42 +0100

    Changed the contact button and added the contact-cards on the /About page

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+const btnModalLudvig = document.querySelector("#btn-modal-ludvig");
+const modalLudvig = document.querySelector("#modal-ludvig");
+
+btnModalLudvig.addEventListener("click", function(){
+    modalLudvig.showModal();
+})
