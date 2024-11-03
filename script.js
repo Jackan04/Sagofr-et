384b2cbeb9875bdf507be0623ae79e6c331f1a71
Jackan04 <jacob.asker@gmail.com>
2024-11-03 13:08:51 +0100

    Commit

--- script.js
+++ script.js
@@ -1,8 +1,69 @@
 
+// Modals for the about-cards
 
+// 1. Ludvig Rosbecker
 const btnModalLudvig = document.querySelector("#btn-modal-ludvig");
 const modalLudvig = document.querySelector("#modal-ludvig");
+const closeModalLudvig = document.querySelector("#close-ludvig");
 
 btnModalLudvig.addEventListener("click", function(){
     modalLudvig.showModal();
+})
+
+closeModalLudvig.addEventListener("click", function(){
+    modalLudvig.close();
+})
+
+// Filip Kjellgren
+
+const btnModalFilip = document.querySelector("#btn-modal-filip");
+const modalFilip = document.querySelector("#modal-filip");
+const closeModalFilip = document.querySelector("#close-filip");
+
+btnModalFilip.addEventListener("click", function(){
+    modalFilip.showModal();
+})
+
+closeModalFilip.addEventListener("click", function(){
+    modalFilip.close();
+})
+
+// Julia Asker
+
+const btnModalJulia = document.querySelector("#btn-modal-julia");
+const modalJulia = document.querySelector("#modal-julia");
+const closeModalJulia = document.querySelector("#close-julia");
+
+btnModalJulia.addEventListener("click", function(){
+    modalJulia.showModal();
+})
+
+closeModalJulia.addEventListener("click", function(){
+    modalJulia.close();
+})
+
+// Alice Tartu
+const btnModalAlice = document.querySelector("#btn-modal-alice");
+const modalAlice = document.querySelector("#modal-alice");
+const closeModalAlice = document.querySelector("#close-alice");
+
+btnModalAlice.addEventListener("click", function(){
+    modalAlice.showModal();
+})
+
+closeModalAlice.addEventListener("click", function(){
+    modalAlice.close();
+})
+
+// Måns Lundin
+const btnModalMans = document.querySelector("#btn-modal-mans");
+const modalMans = document.querySelector("#modal-mans");
+const closeModalMans = document.querySelector("#close-mans");
+
+btnModalMans.addEventListener("click", function(){
+    modalMans.showModal();
+})
+
+closeModalMans.addEventListener("click", function(){
+    modalMans.close();
 })
